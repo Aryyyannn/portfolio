5e3f484f2e08c4e50167ee6ccc396e77acee3313
aryan <aryanbhartitomar@gmail.com>
2026-09-13 17:18:40 +0530

    Use Next 16 proxy

--- ✅ proxy.ts
+++ ✅ proxy.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export function proxy(req: NextRequest) {
+export function middleware(req: NextRequest) {
   if (!req.nextUrl.pathname.startsWith("/admin")) {
     return NextResponse.next();
   }
